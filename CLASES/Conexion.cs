9a99016b0405835617d1e294ac20d66829bf9fab
CARLOS CAICEDO <carlos.caicedo493@pascualbravo.edu.co>
2025-05-19 00:49:55 -0500

    se crea la clase registro de productos en la cual estamos creando la instacias necesarias para guardar los productos

---
+++ CLASES/Conexion.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Data.SqlClient;
+using System.Data;
+using System.Net;
+
+namespace supermecadoelectronico.CLASES
+{
+    class Conexion
+    {
+        SqlConnection sqlcon;
+        SqlCommand cmd;
+        SqlDataAdapter da;
+        DataTable dt;
+
+        public void OPen()
+        {
+            string nombre_servidor = Dns.GetHostName();
+            sqlcon = new SqlConnection("Data Source=" + nombre_servidor + ";Initial Catalog=SupermercadoElectronico;Integrated Security=True");
+        }
+
+
+    }
+}
---
+++ CLASES/RegistroProductos.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace supermecadoelectronico.CLASES
+{
+    public class RegistroProductos
+    {
+        private static RegistroProductos _instancia;
+        private static readonly object _lock = new object();
+        public List<Productos> Inventario { get; private set; }
+
+        private RegistroProductos()
+        {
+            Inventario = new List<Productos>();
+        }
+
+        public static RegistroProductos Instancia
+        {
+            get
+            {
+                lock (_lock)
+                {
+                    if (_instancia == null)
+                        _instancia = new RegistroProductos();
+                    return _instancia;
+                }
+            }
+        }
+
+        public void AgregarPedido(Productos productos)
+        {
+            Inventario.Add(productos);
+        }
+    }
+}
