4d29d456ba808fef85b1a9cd4e05d0da9df4654b
CARLOS CAICEDO <carlos.caicedo493@pascualbravo.edu.co>
2025-05-18 23:50:48 -0500

    se cambia nombre de clase y se añade la regla que los campos no pueden estar vacios

---
+++ CLASES/Productos.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace supermecadoelectronico.CLASES
+{
+    public abstract class Productos
+    {
+        public int IDProductos { get; set; }
+        public string Marca { get; set; }
+        public string Modelo { get; set; }
+        public decimal Precio { get; set; }
+        public int Catidad { get; set; }
+
+
+        public Productos(int IDproductos, string marca, string modelo, decimal precio, int cantidad)
+        {
+            if (string.IsNullOrWhiteSpace(marca))
+            {
+                throw new ArgumentException("la marca no puede estar vacio");
+            }
+            if (string.IsNullOrWhiteSpace(modelo))
+            {
+                throw new ArgumentException("el numero base debe ser mayor a 0");
+            }
+            if (precio <= 0)
+            {
+                throw new ArgumentException("el nuemro de base debe ser mayor a 0");
+            }
+            if (cantidad <= 0)
+            {
+                throw new ArgumentException("el nuemro de base debe ser mayor a 0");
+            }
+
+            Marca = marca;
+            Modelo = modelo;
+            Precio = precio;
+            Catidad = cantidad;
+        }
+
+        public abstract double CalcularCostoTotal();
+
+    }
+}
