de95dc95e9455fd254050dd2a77d9b1405b00937
CARLOS CAICEDO <carlos.caicedo493@pascualbravo.edu.co>
2025-05-24 00:23:38 -0500

    se crea la interfase de proveedores

---
+++ CLASES/repositorios/Iproveedorrepository.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace supermecadoelectronico.CLASES.repositorios
+{
+    public interface Iproveedorrepository
+    {
+        List<Proveedor> ObtenerTodos();
+
+        void Insertar(Proveedor proveedor);
+        void Actualizar(Proveedor proveedor);
+        void Eliminar(int ProveedorID);
+    }
+}
