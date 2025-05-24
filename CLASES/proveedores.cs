c9bbe553b811e297d4b41d772629066ca0c30788
CARLOS CAICEDO <carlos.caicedo493@pascualbravo.edu.co>
2025-05-23 23:34:00 -0500

    se crea la clase de proveedores

---
+++ CLASES/proveedores.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace supermecadoelectronico.CLASES
+{
+    public class proveedores
+    {
+        public int ProductoID {get; set;}
+        public int ProveedorID { get; set;}
+        public string Proveedor { get; set;}
+        public string contacto {  get; set;}
+
+    }
+}
