027699af66e953e0b64327e64bae46f37ad8cc23
CARLOS CAICEDO <carlos.caicedo493@pascualbravo.edu.co>
2025-06-01 21:34:28 -0500

    se crea el service de devoluciones

---
+++ CLASES/services/DevolucionService.cs
@@ -0,0 +1,24 @@
+﻿using supermecadoelectronico.CLASES.repositorios;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace supermecadoelectronico.CLASES.services
+{
+    public class DevolucionService
+    {
+        private readonly IDevolucionRepository _repo;
+
+        public DevolucionService(IDevolucionRepository repo)
+        {
+            _repo = repo;
+        }
+
+        public void RegistrarDevolucion(int ventaId, int productoId, int cantidad)
+        {
+            _repo.RegistrarDevolucion(ventaId, productoId, cantidad);
+        }
+    }
+}
